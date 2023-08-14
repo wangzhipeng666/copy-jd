e7cca40d9701e976277e8c2b8ee8402b9ac5cf28
汪智鹏 <1848993253@qq.com>
2023-08-14 13:18:40 +0800

    文章列表样式

---
+++ my-vue-app/src/components/articleManage/components/list.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-table :data="tableData" style="width: 100%" stripe>
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
+</template>
+
+<script setup>
+import {} from 'vue'
+import { ElTable, ElTableColumn } from 'element-plus';
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
+</script>
+
+<style lang="less" scoped></style>
--- my-vue-app/src/components/articleManage/index.vue
+++ my-vue-app/src/components/articleManage/index.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="article">
         <el-card shadow="always" class="article-search">搜索</el-card>
-        <el-card shadow="always">列表</el-card>
+        <el-card shadow="always">
+            <articleList />
+        </el-card>
     </div>
 </template>
 
 <script setup>
 import { ElCard } from 'element-plus';
+import articleList from './components/list.vue'
 </script>
 
 <style lang="less" scoped>
