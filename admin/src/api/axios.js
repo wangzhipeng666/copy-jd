e85f15620c751ac0c6d0f2c7200341c4d6e8c739
汪智鹏 <1848993253@qq.com>
2023-09-01 14:46:31 +0800

    删除无用代码

--- admin/src/api/axios.js
+++ admin/src/api/axios.js
@@ -10,10 +10,6 @@ const instance = axios.create({
 
 /** 添加请求拦截器 **/
 instance.interceptors.request.use(config => {
-    // 全局设置token
-    // 后期配合服务端同时处理
-    // config.headers['token'] = sessionStorage.getItem('token') || ''
-
     // 添加时间戳参数，防止浏览器（IE）对get请求的缓存
     if (config.method === 'get') {
         config.params = {
@@ -23,11 +19,11 @@ instance.interceptors.request.use(config => {
     }
 
     // // 如果是导出文件的接口：因为返回的是二进制流，所以需要设置请求响应类型为blob。
-    // if (config.url.includes('pur/contract/export')) {
+    // if (config.url.includes('/api/blog/export')) {
     //     config.headers['responseType'] = 'blob'
     // }
     // // 如果是文件上传，发送的是二进制流，所以需要设置请求头的'Content-Type'。
-    // if (config.url.includes('pur/contract/upload')) {
+    // if (config.url.includes('/api/blog/upload')) {
     //     config.headers['Content-Type'] = 'multipart/form-data'
     // }
     return config
