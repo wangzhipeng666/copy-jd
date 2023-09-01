fb66ade69396688f577cda1b3cecf38b711996da
汪智鹏 <1848993253@qq.com>
2023-09-01 14:47:14 +0800

    axios统一处理请求异常

--- admin/src/api/axios.js
+++ admin/src/api/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { ElLoading, ElMessage } from 'element-plus';
 
 const instance = axios.create({
     baseURL: 'http://localhost:8000', // 设置统一的请求前缀
@@ -35,11 +36,15 @@ instance.interceptors.request.use(config => {
 instance.interceptors.response.use(response => {
     if (response.data.errno == '0') {
         return Promise.resolve(response.data)
+    } else {
+        ElMessage({
+            message: response.data.message,
+            type: 'error'
+        })
+        return Promise.reject(response.data.message)
     }
-
-
 }, error => {
-
+    httpErrorStatusHandle(error)
 })
 
 /* 统一封装get请求 */
@@ -71,5 +76,38 @@ export const post = (url, data, config = {}) => {
         }).catch(error => {
             reject(error)
         })
+    })
+}
+
+/**
+ * 处理异常
+ * @param {*} error 
+ */
+function httpErrorStatusHandle(error) {
+    let message = '';
+    if (error && error.message) {
+        switch(error.response.status) {
+            case 302: message = '接口重定向了！';break;
+            case 400: message = '参数不正确！';break;
+            case 401: message = '您未登录，或者登录已经超时，请先登录！';break;
+            case 403: message = '您没有权限操作！'; break;
+            case 404: message = `请求地址出错: ${error.response.config.url}`; break;
+            case 408: message = '请求超时！'; break;
+            case 409: message = '系统已存在相同数据！'; break;
+            case 500: message = '服务器错误！'; break;
+            case 501: message = '服务未实现！'; break;
+            case 502: message = '网关错误！'; break;
+            case 503: message = '服务不可用！'; break;
+            case 504: message = '服务暂时无法访问，请稍后再试！'; break;
+            case 505: message = 'HTTP版本不受支持！'; break;
+            default: message = '异常问题，请联系管理员！';break;
+        }
+    }
+    if (error.message.includes('timeout')) message = '网络请求超时！';
+    if (error.message.includes('Network')) message = window.navigator.onLine ? '服务端异常！' : '您断网了！';
+
+    ElMessage({
+        type: 'error',
+        message
     })
 }
