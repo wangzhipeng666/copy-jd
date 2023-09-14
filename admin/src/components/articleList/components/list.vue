0c009df89b7ae23be81646703ae8f9bb473ff7c1
wangzhipeng <1848993253@qq.com>
2023-09-14 08:59:12 +0800

    文章修改和删除按钮

--- admin/src/components/articleList/components/list.vue
+++ admin/src/components/articleList/components/list.vue
@@ -10,6 +10,12 @@
       </template>
     </el-table-column>
     <el-table-column prop="author" label="author" />
+    <el-table-column label="操作">
+      <template #default="scope">
+        <el-button type="primary" @click="handleEditArticle(scope.row.id)" style="margin-right:10px;">修改</el-button>
+        <el-button type="danger" @click="handleDelArticle(scope.row.id)">删除</el-button>
+      </template>
+    </el-table-column>
   </el-table>
 </template>
 
@@ -36,6 +42,19 @@ const getBlogList = () => {
 
 const handleToPage = () => {
   router.push('/createArticle');
+}
+
+const handleEditArticle = (id) => {
+  router.push({
+    path: '/createArticle',
+    query: {
+      id: JSON.stringify(id)
+    }
+  });
+}
+
+const handleDelArticle = (id) => {
+  console.log(id)
 }
 
 onMounted(() => {
